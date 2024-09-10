762db7d22c3d50ce5bf191b581af2d5e55be30fb
Jonathan Yeong <hey@jonathanyeong.com>
2024-09-10 00:44:56 +0000

    Add basic estructure for the CLI tool

--- main.go
+++ main.go
@@ -5,58 +5,56 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
 )
 
-type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
+// What next - Offer a UI for me to type out what I should be doing next. Stretch goal would be to add the task to a specific project. Find a way to create a project. It could default to the project you're in?
+// What now - If you're in a folder it defaults to show the tasks for that folder. Otherwise, it lets you choose which project to look at. You can also check off things that are done.
+// Next time you do what now, it won't have that task.
+
+type Task struct {
+	Description string
+	Done        bool
 }
 
-func initialModel() model {
-	return model{
-		// Our to-do list is a grocery list
-		choices: []string{"Buy carrots", "Buy celery", "Buy kohlrabi"},
+type Project struct {
+	Name  string
+	Tasks []Task
+}
+type Model struct {
+	Projects []Project
+	cursor   int
+	selected map[int]struct{}
+}
 
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
+func initialModel() Model {
+	return Model{
+		Projects: []Project{
+			{Name: "Default", Tasks: []Task{{Description: "sample task", Done: false}}},
+		},
 		selected: make(map[int]struct{}),
 	}
 }
 
-func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
+func (m Model) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 
-	// Is it a key press?
 	case tea.KeyMsg:
-
-		// Cool, what was the actual key pressed?
 		switch msg.String() {
-
-		// These keys should exit the program.
 		case "ctrl+c", "q":
 			return m, tea.Quit
-
-		// The "up" and "k" keys move the cursor up
 		case "up", "k":
 			if m.cursor > 0 {
 				m.cursor--
 			}
-
-		// The "down" and "j" keys move the cursor down
 		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
+			if m.cursor < len(m.Projects[0].Tasks)-1 {
 				m.cursor++
 			}
-
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
 		case "enter", " ":
 			_, ok := m.selected[m.cursor]
 			if ok {
@@ -66,46 +64,58 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 		}
 	}
-
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
 	return m, nil
 }
 
-func (m model) View() string {
-	// The header
-	s := "What should we buy at the market?\n\n"
-
-	// Iterate over our choices
-	for i, choice := range m.choices {
-
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
+func (m Model) View() string {
+	s := "What Now?\n\n"
+	for i, task := range m.Projects[0].Tasks {
+		cursor := " "
 		if m.cursor == i {
-			cursor = ">" // cursor!
+			cursor = ">"
 		}
 
-		// Is this choice selected?
-		checked := " " // not selected
+		checked := " "
 		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
+			checked = "x"
 		}
 
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, task.Description)
 	}
-
-	// The footer
 	s += "\nPress q to quit.\n"
-
-	// Send the UI for rendering
 	return s
 }
 
+var rootCmd = &cobra.Command{
+	Use:   "what",
+	Short: "A task manager inside your CLI!",
+}
+
+var nextCmd = &cobra.Command{
+	Use:   "next",
+	Short: "Add a new task",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Adding a new task...")
+	},
+}
+
+var nowCmd = &cobra.Command{
+	Use:   "now",
+	Short: "View and manage current tasks",
+	Run: func(cmd *cobra.Command, args []string) {
+		p := tea.NewProgram(initialModel())
+		if _, err := p.Run(); err != nil {
+			fmt.Printf("Error: %v", err)
+			os.Exit(1)
+		}
+	},
+}
+
 func main() {
-	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
+	rootCmd.AddCommand(nextCmd)
+	rootCmd.AddCommand(nowCmd)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
